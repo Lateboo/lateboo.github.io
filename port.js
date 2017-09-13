4e264e6629a97cdf03e765659ad91f8ff1e6e4c6
Parker Henderson <Parker_J@live.com>
2017-09-13 12:13:59 -0700

    Add files via upload

---
+++ port.js
@@ -0,0 +1,14 @@
+<script>
+$(document).ready(function() {
+      
+        $('.gallery-item').magnificPopup({
+          type: 'image',
+          gallery:{
+            enabled:true
+          }
+        });
+        
+    
+        $('#container').mixItUp();
+    });
+</script>
